d3ff064601a83b81613514aaa4463ac1152fb229
rifqinanda27 <rifqinanda60@gmail.com>
2025-05-05 18:10:21 +0700

    update game lives

--- Assets/Scripts/Game 3/BatasAkhirLiga.cs
+++ Assets/Scripts/Game 3/BatasAkhirLiga.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BatasAkhirLiga : MonoBehaviour
 {
+    public static int nyawa = 3;
+
+    public Image heart1;
+    public Image heart2;
+    public Image heart3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        heart1.gameObject.SetActive(true);
+        heart2.gameObject.SetActive(true);
+        heart3.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -18,6 +26,25 @@ public class BatasAkhirLiga : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
-        SceneManager.LoadScene("GameOver");
+        nyawa--;
+
+        UpdateUI();
+
+        if (nyawa <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+            nyawa = 3; // Reset nyawa
+        }
+    }
+
+    void UpdateUI()
+    {
+        // Nonaktifkan hati satu per satu
+        if (nyawa == 2)
+            heart3.gameObject.SetActive(false);
+        else if (nyawa == 1)
+            heart2.gameObject.SetActive(false);
+        else if (nyawa <= 0)
+            heart1.gameObject.SetActive(false);
     }
 }
