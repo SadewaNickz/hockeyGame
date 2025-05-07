9f6d7e0766e62713d0a8e0373a0be5d5a520f5c8
SadewaNickz <Bagussadewa444@gmail.com>
2025-05-07 20:29:05 +0700

    Game 2 Selesai

--- Assets/Scripts/Game 2/BGM2.cs
+++ Assets/Scripts/Game 2/BGM2.cs
@@ -22,7 +22,7 @@ public class BGM2 : MonoBehaviour
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         // Jika masuk ke scene LawanBot atau Main, hancurkan BGM2
-        if (scene.name == "LawanBot" || scene.name == "Main")
+        if (scene.name == "LawanBot" || scene.name == "Main" || scene.name == "MainMenu")
         {
             Destroy(gameObject);
         }
